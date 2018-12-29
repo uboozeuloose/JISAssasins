e7ed0960e4f7dcbd7967e5c81e03eb609108f5db
uboozeuloose <46235934+uboozeuloose@users.noreply.github.com>
2018-12-29 11:08:24 +0530

    Q20

---
+++ Q20.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Write a   program to accept string from the user and replace all occurrences of character ‘a’ by ‘*’ symbol.
+
+
+
+# In[5]:
+
+
+string=input("Enter string:")
+string=string.replace('a','*')
+
+print("Modified string:")
+print(string)
+
+
+# In[ ]:
+
+
+
+
