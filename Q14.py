0974fbfde4d773c81cb9046c48accaba51e4d34e
uboozeuloose <46235934+uboozeuloose@users.noreply.github.com>
2018-12-29 15:11:47 +0530

    Q14

---
+++ Q14.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+str=input("Enter the string")
+char=input("Enter the character whose number of occurences is to be found in the string")
+c=str.count(char)
+print("Number of occurences of {} is {}".format(char,c))
+
+
+# In[ ]:
+
+
+
+
