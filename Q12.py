ac172977e743162409af69028e72a303678bdf64
uboozeuloose <46235934+uboozeuloose@users.noreply.github.com>
2018-12-29 12:50:56 +0530

    Q12

---
+++ Q12.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+
+
+msg="i hate it"
+
+words=msg.split(" ")
+words.reverse()
+print(' '.join(words))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
