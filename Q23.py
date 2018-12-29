cf086bebbfb331aefb3da5f8b38ca07ca4c1dea9
uboozeuloose <46235934+uboozeuloose@users.noreply.github.com>
2018-12-29 15:54:33 +0530

    Q23

---
+++ Q23.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+X = input("Enter your string")
+l=len(X)
+print("The length is", l)
+Y = "Ankita"
+Z= X+Y
+print("After concatenation", Z)
+print(''.join(reversed(X)))
+old="I like Python"
+new=old.replace("like","love")
+print(new)
+if X == Y:
+  print("Equal")
+else:
+  print("Not Equal")
+ 
+
+
+# In[3]:
+
+
+`
+
+
+# In[ ]:
+
+
+
+
