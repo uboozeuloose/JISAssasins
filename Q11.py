55f02f37c43b23c689bfcfb56a6b236f4bc080c9
uboozeuloose <46235934+uboozeuloose@users.noreply.github.com>
2018-12-29 14:01:28 +0530

    Q11

---
+++ Q11.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Program to accept a string from user and accept a char value from user and find out the total occurence of char  value in the string value By Moumita Chakraborty
+str=input("Enter the string")
+char=input("Enter the character whose number of occurences is to be found in the string")
+c=str.count(char)
+print("Number of occurences of {} is {}".format(char,c))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
