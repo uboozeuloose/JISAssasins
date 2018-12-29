b4b9f129d99416f84f3775a36ef2e4bf9324e687
uboozeuloose <46235934+uboozeuloose@users.noreply.github.com>
2018-12-29 14:44:50 +0530

    Q8

---
+++ Q8.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+x=2
+y=5
+
+temp = x
+x = y
+y = temp
+
+print('The value of x after swapping: {}'.format(x))
+print('The value of y after swapping: {}'.format(y))
+
+
+# In[ ]:
+
+
+
+
