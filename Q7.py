243113e6b3c7110badf5481b22eb4f64b392d55b
uboozeuloose <46235934+uboozeuloose@users.noreply.github.com>
2018-12-29 11:47:57 +0530

    Q7

---
+++ Q7.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Write a program to calculate the sum of digits of a given number
+
+
+num = input("Enter a nummber")
+sum = 0
+
+for i in num:
+    sum+=int(i)
+    
+print("the sum of digits of a number",sum)
+
+
+# In[ ]:
+
+
+
+
