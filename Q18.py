058c084242ec28a994de5f19263c80daacb03896
uboozeuloose <46235934+uboozeuloose@users.noreply.github.com>
2018-12-29 14:33:19 +0530

    Q18

---
+++ Q18.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+#Write a program to calculate the x to the power y without using Standard functions.
+
+
+# In[7]:
+
+
+num=int(input("enter a number"))
+num2=int(input("enter 2nd number"))
+a=1
+for num2 in range (1,num2+1):
+    a=(a*num)
+print(a)
+
+
+# In[ ]:
+
+
+
+
