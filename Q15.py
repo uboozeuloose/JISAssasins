99528244bddb27553544dec12128b2966b8234ef
uboozeuloose <46235934+uboozeuloose@users.noreply.github.com>
2018-12-30 14:02:45 +0530

    Q15

---
+++ Q15.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+x=int(input("enter a value"))
+y=int(input("enter another number"))
+z=int(input("enter 3rd number"))
+a=y+z
+print("the result is",x**a)
+
+
+# In[ ]:
+
+
+
+
